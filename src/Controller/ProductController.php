e4afbc15464c54586e40113210df2b22b962bf2f
srabiholi <olivier.0@hotmail.fr>
2021-04-02 16:38:51 +0200

    form category and product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controller;
 
-use App\Repository\CategoryRepository;
+use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProductController extends AbstractController
 {
+
     /**
      * @Route("/{slug}", name="product_category")
      */
@@ -51,4 +57,66 @@ class ProductController extends AbstractController
         ]);
 
     }
+
+
+    /**
+     * @Route("/admin/product/{id}/edit", name="product_edit")
+     */
+    public function edit($id, ProductRepository $productRepository, Request $request,SluggerInterface $slugger, EntityManagerInterface $em)
+    {
+        $product = $productRepository->find($id);
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+            $product->setSlug(strtolower($slugger->slug($product->getName())));
+
+            $em->flush();
+
+            return $this->redirectToRoute('product_show', [
+                'category_slug' => $product->getCategory()->getSlug(),
+                'slug' => $product->getSlug()
+            ]);
+        }
+
+
+        return $this->render('product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/product/create", name="product_create")
+     */
+    public function create(Request $request, SluggerInterface $slugger, EntityManagerInterface $em)
+    {
+        $product = new Product();
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+
+            $product->setSlug(strtolower($slugger->slug($product->getName())));
+
+            $em->persist($product);
+            $em->flush();
+
+            return $this->redirectToRoute('product_show', [
+                'category_slug' => $product->getCategory()->getSlug(),
+                'slug' => $product->getSlug()
+            ]);
+        }
+        
+        
+
+        return $this->render('product/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
 }

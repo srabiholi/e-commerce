39062576b1dc8cff7dec5966a74d5b12100fc9d2
srabiholi <olivier.0@hotmail.fr>
2021-04-05 23:19:44 +0200

    fix validation

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,7 +2,8 @@
 
 namespace App\Entity;
 
-use App\Repository\CategoryRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -21,6 +22,8 @@ class Category
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Le nom de la catégorie est obligatoire")
+     * @Assert\Length(min=3, max=255, minMessage="Le nom de la categorie doit comporter {{ limit }} caratères")
      */
     private $name;
 
